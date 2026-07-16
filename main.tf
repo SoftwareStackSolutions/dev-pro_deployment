569115f4e028c6686df3c9296977c28352894e7c
Ashwini Kanagaraj <153802512+ashwini-k-s@users.noreply.github.com>
2026-07-16 15:15:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.9.8"
+}
+
+resource "terraform_data" "demo" {
+  input = "Hello Dev Pipeline"
+}
+
+output "message" {
+  value = terraform_data.demo.output
+}
